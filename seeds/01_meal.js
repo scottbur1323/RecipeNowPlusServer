cb350dd1f35690224bce12a582de7bd91572571b
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-04-22 15:14:52 -0600

    fixed error

--- seeds/01_meal.js
+++ seeds/01_meal.js
@@ -1,6 +1,6 @@
 
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "meals"; ALTER SEQUENCE meals_id_seq RESTART WITH 13;')
+  return knex.raw('DELETE FROM "meals"; ALTER SEQUENCE meals_id_seq RESTART WITH 4;')
     .then(function () {
       var meal = [{
         id: 1,
@@ -18,69 +18,6 @@ exports.seed = function(knex, Promise) {
         justInCase: ''
       },{
         id: 3,
-        mealName: 'Egg Salad Sandwiches',
-        picURL: '/static/ess.png',
-        instructionsURL: 'https://www.foodnetwork.com/recipes/tyler-florence/egg-salad-sandwich-with-avocado-and-watercress-recipe-1915683',
-        ingredients: '6 eggs, 1/4 cup mayonaise, 1 tsp dijon mustard, 1/2 lemon, 1 loaf bread, 1 avocado, 1/2 bunch watercress',
-        justInCase: ''
-      },{
-        id: 4,
-        mealName: 'Tuna Noodle Casserole',
-        picURL: '/static/tnc.png',
-        instructionsURL: 'https://www.campbells.com/kitchen/recipes/classic-tuna-noodle-casserole/',
-        ingredients: '1 can cream of celery soup, 1/2 cup milk, 1 cup frozen peas, 2 cans of tuna, 4 oz egg noddles, 2 tbsp bread crumbs, 1 tbsp butter, 2 tbsp chopped pimiento',
-        justInCase: ''
-      },{
-        id: 5,
-        mealName: 'BLTs',
-        picURL: '/static/blt.png',
-        instructionsURL: 'http://www.geniuskitchen.com/recipe/classic-blt-sandwich-129615',
-        ingredients: '1/2 lb bacon, 1 loaf bread, 1 head iceberg lettuce, 2 large tomatoes, 1 cup mayonaise',
-        justInCase: ''
-      },{
-        id: 6,
-        mealName: 'Egg Salad Sandwiches',
-        picURL: '/static/ess.png',
-        instructionsURL: 'https://www.foodnetwork.com/recipes/tyler-florence/egg-salad-sandwich-with-avocado-and-watercress-recipe-1915683',
-        ingredients: '6 eggs, 1/4 cup mayonaise, 1 tsp dijon mustard, 1/2 lemon, 1 loaf bread, 1 avocado, 1/2 bunch watercress',
-        justInCase: ''
-      },{
-        id: 7,
-        mealName: 'Tuna Noodle Casserole',
-        picURL: '/static/tnc.png',
-        instructionsURL: 'https://www.campbells.com/kitchen/recipes/classic-tuna-noodle-casserole/',
-        ingredients: '1 can cream of celery soup, 1/2 cup milk, 1 cup frozen peas, 2 cans of tuna, 4 oz egg noddles, 2 tbsp bread crumbs, 1 tbsp butter, 2 tbsp chopped pimiento',
-        justInCase: ''
-      },{
-        id: 8,
-        mealName: 'BLTs',
-        picURL: '/static/blt.png',
-        instructionsURL: 'http://www.geniuskitchen.com/recipe/classic-blt-sandwich-129615',
-        ingredients: '1/2 lb bacon, 1 loaf bread, 1 head iceberg lettuce, 2 large tomatoes, 1 cup mayonaise',
-        justInCase: ''
-      },{
-        id: 9,
-        mealName: 'Egg Salad Sandwiches',
-        picURL: '/static/ess.png',
-        instructionsURL: 'https://www.foodnetwork.com/recipes/tyler-florence/egg-salad-sandwich-with-avocado-and-watercress-recipe-1915683',
-        ingredients: '6 eggs, 1/4 cup mayonaise, 1 tsp dijon mustard, 1/2 lemon, 1 loaf bread, 1 avocado, 1/2 bunch watercress',
-        justInCase: ''
-      },{
-        id: 10,
-        mealName: 'Tuna Noodle Casserole',
-        picURL: '/static/tnc.png',
-        instructionsURL: 'https://www.campbells.com/kitchen/recipes/classic-tuna-noodle-casserole/',
-        ingredients: '1 can cream of celery soup, 1/2 cup milk, 1 cup frozen peas, 2 cans of tuna, 4 oz egg noddles, 2 tbsp bread crumbs, 1 tbsp butter, 2 tbsp chopped pimiento',
-        justInCase: ''
-      },{
-        id: 11,
-        mealName: 'BLTs',
-        picURL: '/static/blt.png',
-        instructionsURL: 'http://www.geniuskitchen.com/recipe/classic-blt-sandwich-129615',
-        ingredients: '1/2 lb bacon, 1 loaf bread, 1 head iceberg lettuce, 2 large tomatoes, 1 cup mayonaise',
-        justInCase: ''
-      },{
-        id: 12,
         mealName: 'Egg Salad Sandwiches',
         picURL: '/static/ess.png',
         instructionsURL: 'https://www.foodnetwork.com/recipes/tyler-florence/egg-salad-sandwich-with-avocado-and-watercress-recipe-1915683',
